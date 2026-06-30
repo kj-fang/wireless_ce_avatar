399226bffe4552adb711392b4733b14d9efae4a5
weiling920131 <weiling920131@gmail.com>
2026-06-30 18:01:02 +0800

    change split file size threshold

--- services/etl_parser/bt_parser.py
+++ services/etl_parser/bt_parser.py
@@ -13,7 +13,7 @@ import re
 # instead of launching a new GUI process every time.
 active_bt_pid = None
 
-SPLIT_SIZE_THRESHOLD_BYTES = 512 * 1024 * 1024  
+SPLIT_SIZE_THRESHOLD_BYTES = 1024 * 1024 * 1024  
 
 
 def _get_true_file_size(path: str) -> int:
